c218840e2639be6a8f9b63aef8915b710f3e8150
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-01-17 19:53:21 -0500

    fixed little grammar issue

--- config.go
+++ config.go
@@ -1,9 +1,10 @@
 package gaea
 
 // TODO: PLEASEEEEEEEEEEEE
+
 // Config ...
 type Config struct {
-	UriConnection  string `yaml:"uri_connection"`
+	URIConnection  string `yaml:"uri_connection"`
 	PeopleDBName   string `yaml:"users_db_name"`
 	AccountsDBName string `yaml:"accounts_db_name"`
 	SessionsDBName string `yaml:"sessions_db_name"`
